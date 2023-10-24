d59624fc2ffadc2b746c65a786c8b666a485842a
Fertilesoil <gasnbloop@gmail.com>
2023-10-23 21:03:06 -0300

    Footer Update

--- blogpessoal/src/pages/footer/Footer.tsx
+++ blogpessoal/src/pages/footer/Footer.tsx
@@ -5,10 +5,12 @@ function Footer() {
       <>
          <footer>
             <div id='footer-flex'>
+               <div className='footer-row'><hr className='hr-footer'/></div>
                <a className='footer-img-format' href="https://github.com/Fertilesoil" target='_blank'>
                   <img src="src\assets\icons8-github-100.svg" alt="Github link" /></a>
                <a className='footer-img-format' href="https://www.linkedin.com/in/fernandodiascosta-dotnet/" target='_blank'>
                   <img src="src\assets\icons8-linkedin-100.svg" alt="Linkedin link" /></a>
+               <div className='footer-row'><hr className='hr-footer'/></div>
             </div>
          </footer>
       </>
